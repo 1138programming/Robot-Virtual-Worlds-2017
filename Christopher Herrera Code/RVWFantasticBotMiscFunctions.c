34b70271c564715d72abad0bf23d2a4a78da4a20
Chris <31458894+ciherrera20@users.noreply.github.com>
2017-11-27 23:11:09 -0800

    Update RVWFantasticBotMiscFunctions.c

--- Christopher Herrera Code/RVWFantasticBotMiscFunctions.c
+++ Christopher Herrera Code/RVWFantasticBotMiscFunctions.c
@@ -1,3 +1,11 @@
+int step = 0;
+float averageEncoderValueAbsolute = 0;
+float rightEncoderValueAbsolute = 0;
+float leftEncoderValueAbsolute = 0;
+float averageEncoderOffset = 0;
+float rightEncoderOffset = 0;
+float leftEncoderOffset = 0;
+
 void init()
 {
 	SensorValue[GyroSensor] = 0;
@@ -13,8 +21,19 @@ void init()
 
 void loopCode()
 {
-	averageEncoderValueAbsolute = (((-1 * SensorValue[RightEncoder]) + SensorValue[LeftEncoder]) / 2);
-	averageEncoderValueRelative = averageEncoderValueAbsolute - encoderOffset;
+	rightEncoderValueAbsolute = -1 * SensorValue[RightEncoder];
+	leftEncoderValueAbsolute = SensorValue[LeftEncoder];
+	//Record the cumulative values of each sensor
+	
+	averageEncoderValueAbsolute = ((rightEncoderValueAbsolute + leftEncoderValueAbsolute) / 2);
+	//Record the average cumulative value of the two sensors
+	
+	rightEncoderValueRelative = rightEncoderValueAbsolute - rightEncoderOffset;
+	leftEncoderValueRelative = leftEncoderValueAbsolute - leftEncoderOffset;
+	//Record the relative values of each sensor
+	
+	averageEncoderValueRelative = averageEncoderValueAbsolute - averageEncoderOffset;
+	//Record the average relative value of the two sensors
 }
 
 void clearStep()
@@ -25,7 +44,9 @@ void clearStep()
 	openClaw(0);
 	wristUp(0);
 	extendArm(0);
-	encoderOffset = averageEncoderValueAbsolute;
+	averageEncoderOffset = averageEncoderValueAbsolute;
+	rightEncoderOffset = rightEncoderValueAbsolute;
+	leftEncoderOffset = leftEncoderValueAbsolute;
 	encoderReference = 0;
 	encoderReferenceFlag = false;
 	step++;
